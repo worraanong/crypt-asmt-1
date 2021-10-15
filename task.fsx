fbeae44288bbbbce2fc87c22efc40e13919ff33a
Worraanong Chanchaijak <wochan@ttu.ee>
2021-10-15 19:24:13 +0300

    Add affine cipher

--- task.fsx
+++ task.fsx
@@ -49,7 +49,23 @@ let indexOfCoincidence (text : string) =
   let N = (float) text.Length
   let compute ni = (ni/N) * (ni-1.0)/(N-1.0)
   ['A'..'Z'] |> Seq.sumBy (fun x-> numberOfOccurrances x text |> float |> compute)
+
+let affineCipherEncrpyt (A:int) (B:int) (plaintext : string) =
+  let compute (x:char) =
+    let X = letterToNum.Item(x) 
+    let result = A*X + B
+    numToLetter.Item(result%26)
+  plaintext |> Seq.iter (compute >> printf "%c")
+
+let affineCipherDecrpyt (A:int) (B:int) (ciphertext : string) =
+  let compute (y:char) =
+    let Y = letterToNum.Item(y) 
+    let result = (A * (Y - B)) % 26
+    let positive = if (result<0) then result+26 else result
+    numToLetter.Item(positive)
+  ciphertext |> Seq.iter (compute >> printf "%c")
   
+
 
 // Assignment tasks
 
@@ -84,3 +100,46 @@ indexOfCoincidence plaintextT2
 indexOfCoincidence "QZAXYLKFLSWMSMOHCALXYMEBPA"
 // 0.03692307692
 
+(**********
+   Task 3
+***********) 
+let plaintextT3  = "SURFACE"
+let ciphertextT3 = "NJCAXTP"
+
+let numP = plaintextT3 |> Seq.map (fun x-> letterToNum.Item(x))
+numP |> Seq.iter (printf "%A ")
+// 18 20 17 5 0 2 4 
+let numC = ciphertextT3 |> Seq.map (fun x-> letterToNum.Item(x))
+numC |> Seq.iter (printf "%A ")
+// 13 9 2 0 23 19 15
+let differences = Seq.zip numC numP |> Seq.map (fun (x,y) -> x - y)
+differences |> Seq.iter (printf "%A ")
+// -5 -11 -15 -5 23 17 11
+
+// Find Encryption key 
+// 'A' -> 'X'
+// AX + B = 23
+// 0 * X + B = 23
+// B = 23 
+// 'C' -> 'T'
+// A * 2 + 23 = 19
+// A = -4/2
+// A = -2
+// But we don't want negative X value; add 26
+// A * 2 + 23 = 19 + 26
+// A = 22/2
+// A = 11
+
+// 11X + 23
+// Verify
+affineCipherEncrpyt 11 23 plaintextT3 
+// NJCAXTP
+
+// Find Decryption key
+// 1 = (11 * A) % 26 
+// A = 45
+// 'X' -> 'A'
+// 45(23 - B) = 0
+// B = 23
+affineCipherDecrpyt 45 23 ciphertextT3
+// SURFACE
