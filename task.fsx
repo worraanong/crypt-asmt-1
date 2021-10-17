02ad1800ec932b87ebc7e95ed95c6ec83fa89a25
Worraanong Chanchaijak <wochan@ttu.ee>
2021-10-18 02:29:20 +0300

    seperate to module

--- task.fsx
+++ task.fsx
@@ -1,73 +1,6 @@
-(*
-    A collection of useful functions to help solving cipher
-    Note that, this is not a complete program 
-*)
+// Assignment 
 
-// Create a dictionary of Alphabet and its corresponding numeric value
-let letterToNum = List.zip ['A'..'Z'] [0..25] |> dict
-let numToLetter = List.zip [0..25] ['A'..'Z'] |> dict
-
-// Return a letter after shift z position; numeric value as key
-let shift (x : char) (z : int) =
-  let shifted = letterToNum.Item(x) + z
-  numToLetter.Item(shifted % 26) // n = 26 letters 
-
-// Return a letter after shift by y; another letter as key
-let shiftBy (x : char) (y : char) =
-  let shifted = letterToNum.Item(x) + letterToNum.Item(y)
-  numToLetter.Item(shifted % 26) // n = 26 letters 
-
-// Shift Cipher
-// 1. Iterate throught each letter in plaintext
-// 2. Shift each letter by key 
-let shiftCipherEncrypt key plaintext =
-  plaintext |> Seq.iter (fun x-> shift x key |> printf "%c")
-
-// VigenereCipherEncrypt
-// 1. Repeat a key until the length is longer or equal to plaintext
-// 2. Zip them together as keyPair in form of [(p0, k0); (p1, k1); ... (pn, kn);]
-// 3. Iterate throught each pair
-// 4. Shift each letter by its own key 
-let vigenereCipherEncrypt (key: string) (plaintext : string) =
-  let repeatedTime = plaintext.Length / key.Length + 1
-  let keys = Seq.toList key
-  let repeatedKeys = List.replicate repeatedTime keys |> List.concat
-  let keyPair = Seq.zip plaintext repeatedKeys
-  keyPair |> Seq.iter (fun (x,y)-> shiftBy x y|> printf "%c")
-
-let numberOfOccurrances (letter: char) (plaintext : string) =
-  Seq.filter (fun x -> x = letter) plaintext |> Seq.length 
-
-(*IndexOfCoincidence
-  1. Calculate per each letter 
-     let na = (float) (numberOfOccurrances 'A' text)
-     let  N = (float) text.Length
-     let Na = (na/N) * (na-1.0)/(N-1.0)
-  2. Sum Na ... Nz
-*)
-let indexOfCoincidence (text : string) =
-  let N = (float) text.Length
-  let compute ni = (ni/N) * (ni-1.0)/(N-1.0)
-  ['A'..'Z'] |> Seq.sumBy (fun x-> numberOfOccurrances x text |> float |> compute)
-
-let affineCipherEncrpyt (A:int) (B:int) (plaintext : string) =
-  let compute (x:char) =
-    let X = letterToNum.Item(x) 
-    let result = A*X + B
-    numToLetter.Item(result%26)
-  plaintext |> Seq.iter (compute >> printf "%c")
-
-let affineCipherDecrpyt (A:int) (B:int) (ciphertext : string) =
-  let compute (y:char) =
-    let Y = letterToNum.Item(y) 
-    let result = (A * (Y - B)) % 26
-    let positive = if (result<0) then result+26 else result
-    numToLetter.Item(positive)
-  ciphertext |> Seq.iter (compute >> printf "%c")
-  
-
-
-// Assignment tasks
+open Cryptography
 
 (**********
    Task 1
@@ -86,6 +19,7 @@ shiftCipherEncrypt ks2 P1
 
 // P2 is done by hand
 
+
 (**********
    Task 2
 ***********) 
@@ -99,6 +33,7 @@ indexOfCoincidence plaintextT2
 
 indexOfCoincidence "QZAXYLKFLSWMSMOHCALXYMEBPA"
 // 0.03692307692
+
 
 (**********
    Task 3
@@ -141,5 +76,35 @@ affineCipherEncrpyt 11 23 plaintextT3
 // 'X' -> 'A'
 // 45(23 - B) = 0
 // B = 23
+
+// 45(Y-23)
+// Verify
 affineCipherDecrpyt 45 23 ciphertextT3
 // SURFACE
+
+
+(**********
+   Task 4
+***********) 
+let m1 = "DOUGH"
+let binaryM1 = letterToBinary m1
+//  ["11"; "1110"; "10100"; "110"; "111"]
+
+let fm1 = "0001101110101000011000111"
+let c1  = "1000000110001010001000100"
+
+// Find the key
+
+let key = oneTimePadEncrypt fm1 c1
+// "1001101000100010010000011"
+
+let m2 = "GLORY"
+let binaryM2 = letterToBinary m2
+// ["110"; "1011"; "1110"; "10001"; "11000"]
+
+let fm2 = "0011001011011101000111000"
+let c2 = oneTimePadEncrypt key fm2
+// "1010100011111111010111011"
+
+let ciphertextT4 = binaryToLetter "10101 00011 11111 10101 11011"
+// "VDFVB"
